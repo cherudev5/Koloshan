d182e8b5528530de0c6ebe4ae04f077a808bcd98
cherudev5 <cheruiyotscripts@gmail.com>
2026-08-09 22:11:17 +0300

    updated testimonial avatar

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -3,7 +3,7 @@ const testimonials = [
     id: 1,
     name: "James Mwangi",
     company: "Nairobi Tech Solutions",
-    image: "/images/testimonials/client1.jpg",
+    image: "/images/testimonials/user.png",
     rating: 5,
     comment:
       "The print quality exceeded our expectations. Our brochures and banners looked fantastic, and delivery was on time.",
@@ -13,7 +13,7 @@ const testimonials = [
     id: 2,
     name: "Grace Wanjiku",
     company: "Grace Events",
-    image: "/images/testimonials/client2.jpg",
+    image: "/images/testimonials/user.png",
     rating: 5,
     comment:
       "Professional service from start to finish. They designed and printed all our event branding beautifully.",
@@ -23,7 +23,7 @@ const testimonials = [
     id: 3,
     name: "David Kiptoo",
     company: "Peak Construction",
-    image: "/images/testimonials/client3.jpg",
+    image: "/images/testimonials/user.png",
     rating: 5,
     comment:
       "Excellent customer service and very competitive pricing. Highly recommend for corporate printing.",
@@ -33,7 +33,7 @@ const testimonials = [
     id: 4,
     name: "Mercy Atieno",
     company: "Creative Hub",
-    image: "/images/testimonials/client4.jpg",
+    image: "/images/testimonials/user.png",
     rating: 5,
     comment:
       "Our branded T-shirts and promotional materials were produced quickly and looked amazing.",
